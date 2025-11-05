3f61ca321c832207e9b2a252ddbed9d02ba77c1f
GaziWani1 <gaziwani123@gmail.com>
2025-11-05 17:17:38 +0530

    selection sort cpp

---
+++ TreesJs/dfs.js
@@ -0,0 +1,40 @@
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const root = new TreeNode('a');
+const nodeB = new TreeNode('b');
+const nodeC = new TreeNode('c');
+const nodeD = new TreeNode('d');
+const nodeE = new TreeNode('e');
+const nodeF = new TreeNode('f');
+
+root.left = nodeB;
+root.right = nodeC;
+nodeB.left = nodeD;
+nodeB.right = nodeE;
+nodeC.left = nodeF;
+
+const bfs = (r) => {
+    if (!r) return [];
+    const queue = [r];
+    const result = []
+    while (queue.length > 0) {
+        const current = queue.shift();
+        result.push(current.value);
+
+        if (current.left) queue.push(current.left)
+        if (current.right) queue.push(current.right)
+    }
+    return result
+}
+
+
+
+const res = bfs(root);
+console.log(res);
+
--- TreesJs/tree.js
+++ TreesJs/tree.js
@@ -19,27 +19,45 @@ nodeB.left = nodeD;
 nodeB.right = nodeE;
 nodeC.left = nodeF;
 
+// const depthFirstTraversal = (r) => {
+//     if (!r) return [];
+
+//     const result = [];
+//     const stack = [];
+
+//     stack.push(r);
+//     while (stack.length > 0) {
+//         const current = stack.pop();
+
+//         result.push(current.value);
+
+//         if (current.right) {
+//             stack.push(current.right)
+//         }
+//         if (current.left) {
+//             stack.push(current.left)
+//         }
+//     }
+
+//     return result;
+// }
+
+// using recursion
 const depthFirstTraversal = (r) => {
-    if (!r) return [];
 
     const result = [];
-    const stack = [];
 
-    stack.push(r);
-    while (stack.length > 0) {
-        const current = stack.pop();
-
-        result.push(current.value);
-
-        if (current.right) {
-            stack.push(current.right)
-        }
-        if (current.left) {
-            stack.push(current.left)
+    const treverse = (node) => {
+        if (node !== null) {
+            result.push(node.value);
+            treverse(node.left);
+            treverse(node.right);
         }
     }
 
+    treverse(r)
     return result;
+
 }
 
 const res = depthFirstTraversal(root);
