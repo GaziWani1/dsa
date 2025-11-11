e5f13e498e1f697e23c39b806433eeffecf89a15
GaziWani1 <gaziwani123@gmail.com>
2025-11-11 20:27:20 +0530

    quick sort

---
+++ recursion/qucikSort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 9;
+    int arr[0] = { 10,
+                   16,
+                   8,
+                   12,
+                   15,
+                   6,
+                   3,
+                   9,
+                   5,
+                   NULL }
+}
