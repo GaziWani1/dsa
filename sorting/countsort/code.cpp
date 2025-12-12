785cbc114ebd98f147f8813ecaf5fe01e8a2f0e0
GaziWani1 <gaziwani123@gmail.com>
2025-12-13 02:19:22 +0530

    redix sort

--- sorting/countsort/code.cpp
+++ sorting/countsort/code.cpp
@@ -20,6 +20,9 @@ void countSort(int max, int arr[], int n)
         cntArr[i] += cntArr[i - 1];
     }
 
+    // for (int i = 0; i < max; i++)
+    //     cout << cntArr[i] << " ";
+
     for (int i = n - 1; i >= 0; i--)
     {
         ans[cntArr[arr[i]] - 1] = arr[i];
--- sorting/redixsort/code.cpp
+++ sorting/redixsort/code.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
 using namespace std;
 
+int getMax(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+    return max;
+}
+
+void countSort(int arr[], int n, int place)
+{
+    int freqArray[10] = {0};
+
+    for (int i = 0; i < n; i++)
+        freqArray[(arr[i] / place) % 10]++;
+
+    // CUMULATIVE FREQUENCE
+    for (int i = 1; i < 10; i++)
+    {
+        freqArray[i] += freqArray[i - 1];
+    }
+
+    int ans[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        int digit = (arr[i] / place) % 10;
+        ans[--freqArray[digit]] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+        arr[i] = ans[i];
+}
+
+void radixSort(int arr[], int n)
+{
+    int max = getMax(arr, n);
+
+    for (int place = 1; max / place > 0; place *= 10)
+        countSort(arr, n, place);
+}
+
 int main()
 {
-    int n = 10;
-    int arr[n] = {12, 48, 21, 30, 17, 93, 88, 69, 72, 53};
+    const int n = 10;
+    int arr[n] = {12, 48, 21, 30, 17, 93, 880, 69, 72, 53};
+
+    radixSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << endl;
+
     return 0;
 }
